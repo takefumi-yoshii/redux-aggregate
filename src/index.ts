20b621d3f74791f8e9c01f8b6c59db16987a63cb
takefumi-yoshii <takefumi.yoshii@dena.com>
2018-03-26 21:22:53 +0900

    update typings

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,11 @@
 import immer from 'immer'
-import { Reducer, AnyAction } from 'redux'
 
 // ______________________________________________________
 
-export interface state {
-  [s: string]: any
-}
-export interface computed {
-  [s: string]: (p?: any) => any
-}
-export interface actions {
-  [s: string]: (p?: any) => void
-}
-export interface Model<I> extends state, computed {}
-export type Modeler<I> = <I>(i: object) => Model<I>
-export interface types {
-  [s: string]: string
-}
-export interface creators {
-  [s: string]: <P>(p?: P) => void
-}
-export interface createActions {
-  types: types
-  creators: creators
-}
-export interface Domain {
-  state?: state
-  computed?: computed
-  actions?: actions
-}
-export interface Aggregate {
-  types: types
-  creators: creators
-  reducer: <I>(modeler: Modeler<I>) => Reducer<Model<I>>
-  modeler: <I>(i: object) => Modeler<I>
-}
-
-// ______________________________________________________
-
-function createActions(ctx: string, __fnns__: string[]) {
-  const types: types = {}
-  const creators: creators = {}
-  __fnns__.map(row => {
+function createActions(ctx, __fnnames__) {
+  const types = {}
+  const creators = {}
+  Object.keys(__fnnames__).forEach(row => {
     const type = `${ctx}${row}`
     types[row] = type
     creators[row] = payload => ({ type, payload })
@@ -49,13 +13,13 @@ function createActions(ctx: string, __fnns__: string[]) {
   return { types, creators }
 }
 
-function createReducer(__acts__: actions) {
-  return function <I>(initialModel: Modeler<I>): Reducer<Modeler<I>> {
-    return (model = initialModel, action: AnyAction): Modeler<I> => {
-      const { type, payload } = action
-      if (typeof __acts__[type] !== 'function') return model
+function createReducer(__actions__) {
+  return function (initialModel) {
+    return (model = initialModel, action) => {
+      if (typeof __actions__[action.type] !== 'function') return model
+      const payload = action.payload || {}
       return immer(model, draft => {
-        __acts__[action.type].bind(draft)(payload)
+        __actions__[action.type].bind(draft)(payload)
       })
     }
   }
@@ -63,21 +27,21 @@ function createReducer(__acts__: actions) {
 
 // ______________________________________________________
 
-export function createAggregate(ctx: string, domain: Domain): Aggregate {
+export function createAggregate(ctx, domain) {
   const { state, computed, actions } = domain
-  const __acts__: actions = {}
-  const __fnns__: string[] = []
+  const __actions__ = {}
+  const __fnnames__ = {}
   Object.keys(actions).forEach(key => {
-    __acts__[`${ctx}${key}`] = actions[key]
-    __fnns__.push(key)
+    __actions__[`${ctx}${key}`] = actions[key]
+    __fnnames__[key] = key
   })
-  const { types, creators } = createActions(ctx, __fnns__)
-  const reducer = createReducer(__acts__)
-  const modeler = <I>(injects): Modeler<I> => (<any>Object).assign({}, state, injects, computed)
+  const { types, creators } = createActions(ctx, __fnnames__)
+  const reducer = createReducer(__actions__)
+  const modeler = injects => (<any>Object).assign({}, state, injects, computed)
   return { types, creators, reducer, modeler }
 }
 
-export function reduceAggregate<D, I>(aggregate: Aggregate, injects?: object): Reducer<Model<I>> {
+export function reduceAggregate(aggregate, injects) {
   const { reducer, modeler } = aggregate
   return reducer(modeler(injects))
 }
