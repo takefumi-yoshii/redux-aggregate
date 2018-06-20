cd1b6fbf5d133d800917d04b1083d5e7a97ab328
takefumi-yoshii <takefumi.yoshii@dena.jp>
2018-06-20 22:25:26 +0900

    add strict mutation type check

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { Reducer } from 'redux'
 
+type A1<T> = T extends (a1: infer I, ...rest: any[]) => any ? I : never
 type A2<T> = T extends (a1: any, a2: infer I, ...rest: any[]) => any ? I : never
-type Types<M> = {readonly [S in keyof M]: string}
+type Types<T> = {readonly [K in keyof T]: string}
 type Payload<T> = T extends A2<T> ? never : A2<T>
 type CreatorReturn<T> = T extends A2<T> ? { type: string } : { type: string, payload: A2<T> }
 type Creator<T> = T extends A2<T> ? () => CreatorReturn<T> : (payload: Payload<T>) => CreatorReturn<T>
-type Creators<M> = {readonly [S in keyof M]: Creator<M[S]>}
+type Creators<T> = {readonly [K in keyof T]: Creator<T[K]>}
+type Mutation<T> = T extends A2<T> ? (state: A1<T>) => A1<T> : (state: A1<T>, payload: A2<T>) => A1<T>
+type Mutations<T> = {readonly [K in keyof T]: Mutation<T[K]>}
 interface Aggregate<M> {
   readonly types: Types<M>
   readonly creators: Creators<M>
@@ -14,7 +17,7 @@ interface Aggregate<M> {
 
 const namespaced = {}
 
-export function createAggregate<M>(mutations: M, namespace: string): Aggregate<M> {
+export function createAggregate<M extends Mutations<M>>(mutations: M, namespace: string): Aggregate<M> {
   if (namespaced[namespace] !== undefined) {
     throw new Error(`redux-aggregate: conflict namespace -> ${namespace}`)
   } else {
