2ffe01dd459edae75ffa5f5bc3d3ca68424ac87f
takefumi-yoshii <takefumi.yoshii@dena.jp>
2018-06-22 16:39:19 +0900

    add modeler

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,35 @@
 import { Reducer } from 'redux'
 
-type A1<T> = T extends (a1: infer I, ...rest: any[]) => any ? I : never
-type A2<T> = T extends (a1: any, a2: infer I, ...rest: any[]) => any ? I : never
-type Types<T> = KeyRefMap & {readonly [K in keyof T]: string}
-type Payload<T> = T extends A2<T> ? never : A2<T>
-type CreatorReturn<T> = T extends A2<T> ? { type: string } : { type: string, payload: A2<T> }
-type Creator<T> = T extends A2<T> ? () => CreatorReturn<T> : (payload: Payload<T>) => CreatorReturn<T>
-type Creators<T> = KeyRefMap & {readonly [K in keyof T]: Creator<T[K]>}
-type Mutation<T> = T extends A2<T> ? (state: A1<T>) => A1<T> : (state: A1<T>, payload: A2<T>) => A1<T>
-type Mutations<T> = KeyRefMap & {readonly [K in keyof T]: Mutation<T[K]>}
-interface KeyRefMap { [K: string]: any }
-interface Aggregate<M> {
+type A1<T>          = T extends (a1: infer I, ...rest: any[]) => any ? I : never
+type A2<T>          = T extends (a1: any, a2: infer I, ...rest: any[]) => any ? I : never
+type Payload<T>     = T extends A2<T> ? never : A2<T>
+type CreatorR<T>    = T extends A2<T> ? { type: string } : { type: string, payload: A2<T> }
+type Creator<T>     = T extends A2<T> ? () => CreatorR<T> : (payload: Payload<T>) => CreatorR<T>
+type Mutation<T>    = T extends A2<T> ? (state: A1<T>) => A1<T> : (state: A1<T>, payload: A2<T>) => A1<T>
+type Types<T>       = { readonly [K in keyof T]: string }
+type Creators<T>    = { readonly [K in keyof T]: Creator<T[K]> }
+type Mutations<T>   = { readonly [K in keyof T]: Mutation<T[K]> }
+type ReducerFactory = <S>(state: S) => Reducer<S>
+type KeyMap         = { [K: string]: any }
+export interface Aggregate<M> {
   readonly types: Types<M>
   readonly creators: Creators<M>
-  readonly reducerFactory: <S>(state: S) => Reducer<S>
+  readonly reducerFactory: ReducerFactory
 }
+type Injects<T> = { [P in keyof T]?: T[P] }
+export type Modeler<T> = (injects?: Injects<T>) => T
 
-const namespaced: KeyRefMap = {}
+const namespaced: KeyMap = {}
 
-export function createAggregate<M extends Mutations<M>>(mutations: M, namespace: string): Aggregate<M> {
+export function createAggregate<M extends KeyMap & Mutations<M>>(mutations: M, namespace: string): Aggregate<M> {
   if (namespaced[namespace] !== undefined) {
     throw new Error(`redux-aggregate: conflict namespace -> ${namespace}`)
   } else {
     namespaced[namespace] = namespace
   }
-  const types: KeyRefMap = {}
-  const creators: KeyRefMap = {}
-  const mutators: KeyRefMap = {}
+  const types: KeyMap = {}
+  const creators: KeyMap = {}
+  const mutators: KeyMap = {}
   Object.keys(mutations).forEach(mutationKey => {
     const type = `${namespace}${mutationKey}`
     types[mutationKey] = type
@@ -43,6 +46,6 @@ export function createAggregate<M extends Mutations<M>>(mutations: M, namespace:
   return {
     types: (types as Types<M>),
     creators: (creators as Creators<M>),
-    reducerFactory
+    reducerFactory: (reducerFactory as ReducerFactory)
   }
 }
