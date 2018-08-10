ab8a2f65c43ceaff9959094df96d817b2192fdc6
takefumi-yoshii <takefumi.yoshii@dena.jp>
2018-08-10 20:58:39 +0900

    add many-to-many usecase

---
+++ examples/todos/src/actions/timer.ts
@@ -0,0 +1,10 @@
+function tick () {
+  const date = new Date()
+  const month = date.getMonth() + 1
+  const _date = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+  return `${month}/${_date} ${hour}:${minute}:${second}`
+}
+export const TimerAC = { tick }
--- examples/todos/src/store.ts
+++ examples/todos/src/store.ts
@@ -1,26 +1,46 @@
 import { createStore, combineReducers, Store, ReducersMapObject } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { createAggregate } from 'redux-aggregate'
-import { TodosST, TodosMT, TodosModel } from './models/todos'
+import { createAggregate, createActions } from '../../../src'
+import { TimerAC } from './actions/timer'
+import { TodosST, TodosMT, TodosModel, TodosSB } from './models/todos'
+import { wait } from './helper/promise'
 
 // ______________________________________________________
+//
+// @ Types
 
 export interface StoreST {
   todos: TodosST
 }
 
 // ______________________________________________________
+//
+// @ Aggregates
 
+export const Timer = createActions(TimerAC, 'timer/')
 export const Todos = createAggregate(TodosMT, 'todos/')
+Todos.subscribe(Timer, TodosSB.Timer)
 
 // ______________________________________________________
+//
+// @ Store
 
 function storeFactory<R extends ReducersMapObject>(reducer: R): Store<StoreST> {
   return createStore(combineReducers(reducer), composeWithDevTools())
 }
-
-// ______________________________________________________
-
 export const store = storeFactory({
   todos: Todos.reducerFactory(TodosModel({ name: 'TODOS' }))
 })
+
+// ______________________________________________________
+//
+// @ Services
+
+async function runTimerService() {
+  while(true) {
+    await wait()
+    store.dispatch(Timer.creators.tick())
+  }
+}
+
+runTimerService()
