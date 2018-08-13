a2009e78a80581a79f2c99613f22cd21d610279e
takefumi-yoshii <takefumi.yoshii@dena.jp>
2018-08-13 10:20:19 +0900

    fix conditional type

--- typings/createActions.d.ts
+++ typings/createActions.d.ts
@@ -3,14 +3,14 @@ import { ActionType, ActionTypes } from './commons'
 
 // ______________________________________________________
 
-type ACS<T> = (payload?: any) => R<T>
+type ACS<T> = () => R<T>
 type ACSPL<T> = (payload: A1<T>) => R<T>
 type ActionSrc<T> = ACS<T> | ACSPL<T>
 type ActionsSrc<T> = { readonly [K in keyof T]: ActionSrc<T[K]> }
 
 type CR<T> = () => { type: ActionType }
 type CRPL<T> = (payload: A1<T>) => { type: ActionType; payload: R<T> }
-type ActionCreator<T> = A1<T> extends never ? CR<T> : CRPL<T>
+type ActionCreator<T> = T extends ACS<T> ? CR<T> : CRPL<T>
 type ActionCreators<T> = { readonly [K in keyof T]: ActionCreator<T[K]> }
 
 // ______________________________________________________
