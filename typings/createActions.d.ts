4ada2c8f9cbf6ea9c7e005a32ad62775c90ebb34
takefumi-yoshii <takefumi.yoshii@dena.jp>
2018-08-15 13:55:44 +0900

    fix return type miss

--- typings/createActions.d.ts
+++ typings/createActions.d.ts
@@ -8,7 +8,7 @@ type ACSPL<T> = (payload: A1<T>) => R<T>
 type ActionSrc<T> = ACS<T> | ACSPL<T>
 type ActionsSrc<T> = { readonly [K in keyof T]: ActionSrc<T[K]> }
 
-type CR<T> = () => { type: ActionType }
+type CR<T> = () => { type: ActionType; payload: R<T> }
 type CRPL<T> = (payload: A1<T>) => { type: ActionType; payload: R<T> }
 type ActionCreator<T> = T extends ACS<T> ? CR<T> : CRPL<T>
 type ActionCreators<T> = { readonly [K in keyof T]: ActionCreator<T[K]> }
